fdc077972b27877d5a1625dd408120cd0e374a2b
Jiaqian Li <nicoleli1231@gmail.com>
2023-07-29 17:03:25 -0400

    javafx

--- README.md
+++
@@ -1,2 +0,0 @@
-# JavaAssign4
- 
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ca.georgiancollege.jl.javaassign4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ca.georgiancollege.jl.javaassign4 to javafx.fxml;
+    exports ca.georgiancollege.jl.javaassign4;
+}
